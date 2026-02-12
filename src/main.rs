cd03203b60418be296b8e36e9474bf0daeac000e
FedYou <federicochurruarin0@gmail.com>
2026-02-11 23:49:37 -0300

    feat: adds base for using ratatui and crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use crossterm::event::KeyCode;
+
+fn main() -> Result<(), std::io::Error> {
+    let mut terminal = ratatui::init();
+
+    loop {
+        if let crossterm::event::Event::Key(key) = crossterm::event::read()? {
+            if key.code == KeyCode::Char('q') {
+                break;
+            }
+        }
+    }
+
+    ratatui::restore();
+    return Ok(());
+}
