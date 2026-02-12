ccc36e652734f561f61c51fa6aacca462711fb1d
FedYou <federicochurruarin0@gmail.com>
2026-02-12 01:28:15 -0300

    feat: add the struct App for better control

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use crossterm::event::KeyCode;
 
+struct App {
+    exit: bool,
+}
+
+impl App {
+    fn render(&mut self, frame: &mut ratatui::Frame) {}
+}
+
 fn main() -> Result<(), std::io::Error> {
     let mut terminal = ratatui::init();
+    let mut app = App { exit: false };
 
     loop {
+        if app.exit {
+            break;
+        }
+
+        let _ = terminal.draw(|frame| app.render(frame));
+
         if let crossterm::event::Event::Key(key) = crossterm::event::read()? {
             if key.code == KeyCode::Char('q') {
-                break;
+                app.exit = true
             }
         }
     }
